10d79c90d60512fb84832862177e053799fee9b7
“Bohdana <bohdana.dev@gmail.com>
2024-07-29 21:41:56 +0300

    Updated dependencies

--- src/config/orm.config.prod.ts
+++ src/config/orm.config.prod.ts
@@ -1,6 +1,5 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { Event } from './../events/event.entity';
 
 export default registerAs(
   'orm.config',
@@ -11,12 +10,7 @@ export default registerAs(
     username: 'root',
     password: 'example',
     database: 'nest-events',
-    //host: process.env.DB_HOST,
-    //port: Number(process.env.DB_PORT),
-    //username: process.env.DB_USER,
-    //password: process.env.DB_PASSWORD,
-    //database: process.env.DB_NAME,
-    entities: [Event],
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: false,
     dropSchema: false,
   }),
